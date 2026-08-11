8ccba73a3311a03115ecf9ff1e0db41b7438e0ec
AnujKumar-tech <anujkumarsingh3458@gmail.com>
2026-08-11 11:31:32 +0530

    Time: 3 ms (29.71%) | Memory: 46.8 MB (37.46%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,19 +1,23 @@
 class Solution {
-
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->{
+            int valueComparison = Integer.compare(a[0],b[0]);
 
-        for (int j = 0; j < k; j++) {
-            int minIndex = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] < nums[minIndex]) {
-                    minIndex = i;
-                }
+            if(valueComparison == 0){
+                return Integer.compare(a[1], b[1]);
             }
-
-            nums[minIndex] *= multiplier;
+            return valueComparison;
+        });
+        for(int i=0; i<nums.length; i++){
+            pq.add(new int[] {nums[i],i});
         }
+        while(k -- > 0){
+            int smallest[] = pq.remove();
+            int index = smallest[1];
 
+            nums[index] *= multiplier;
+            pq.add(new int[]{nums[index],index});
+        }
         return nums;
     }
 }
